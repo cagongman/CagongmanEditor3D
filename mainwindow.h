7a4d5e1f9b4ddd0f7f5bae5c7381de797177ea12
Kisub Lee <kisublee@huvitz.com>
2025-03-13 21:13:10 +0900

    Qt OpenGL CMake 설정

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,19 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions>
+
+class OpenGLTriangleWidget : public QOpenGLWidget, protected QOpenGLFunctions {
+    Q_OBJECT
+public:
+    explicit OpenGLTriangleWidget(QWidget *parent = nullptr);
+
+protected:
+    void initializeGL() override;
+    void resizeGL(int w, int h) override;
+    void paintGL() override;
+};
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
